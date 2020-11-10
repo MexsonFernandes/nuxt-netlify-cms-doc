9693c35cfda44438cda0801be6cc90e0ed7941d4
Mexson Fernandes <robomex2020@gmail.com>
2020-11-10 12:59:46 +0530

    :fire: Add initial netlify config

---
+++ README.md
@@ -0,0 +1,9 @@
+# Nuxt Netlify Doc Template
+
+A base template to create a documentation website using Nuxt.js framework with Netlify CMS support.
+
+## References
+
+* [Netlify CMS documentation](https://www.netlifycms.org/docs/nuxt/)
+* [Jamstack documentation sites](https://www.stackbit.com/blog/jamstack-documentation-sites/)
+* [Nuxtjs/content Example](https://github.com/nuxt/content/blob/dev/example)
---
+++ nuxt.config.js
@@ -0,0 +1,53 @@
+export default {
+  // Target (https://go.nuxtjs.dev/config-target)
+  target: 'static',
+
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  head: {
+    title: 'nuxt-netlify-doc',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  // Auto import components (https://go.nuxtjs.dev/config-components)
+  components: true,
+
+  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+  ],
+
+  // Modules (https://go.nuxtjs.dev/config-modules)
+  modules: [
+    // https://go.nuxtjs.dev/content
+    '@nuxt/content',
+  ],
+
+  // Content module configuration (https://go.nuxtjs.dev/config-content)
+  content: {},
+
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  build: {},
+  hooks: {
+    'content:file:beforeInsert': async (document, database) => {
+      if (document.extension === '.json' && document.body) {
+        const data = await database.markdown.toJSON(document.body)
+
+        Object.assign(document, data)
+      }
+    },
+  },
+  content: {
+    nestedProperties: ['categories.slug'],
+    extendParser: {
+      '.custom': (file) => ({
+        body: file.split('\n').map((line) => line.trim()),
+      }),
+    },
+  },
+}
