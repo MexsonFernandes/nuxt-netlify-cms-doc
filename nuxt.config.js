c64d1cd3e953901a741f4c4ce4382b2ca26cdacc
Mexson Fernandes <robomex2020@gmail.com>
2020-11-11 23:56:50 +0530

    :fire: Add i18n support ready

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,50 +1,58 @@
-export default {
-  // Target (https://go.nuxtjs.dev/config-target)
-  target: 'static',
+// export default {
+//   // Target (https://go.nuxtjs.dev/config-target)
+//   target: 'static',
 
-  // Global page headers (https://go.nuxtjs.dev/config-head)
-  head: {
-    title: 'nuxt-netlify-doc',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-    ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+//   // Global page headers (https://go.nuxtjs.dev/config-head)
+//   head: {
+//     title: 'nuxt-netlify-doc',
+//     meta: [
+//       { charset: 'utf-8' },
+//       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+//       { hid: 'description', name: 'description', content: '' },
+//     ],
+//     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+//   },
+
+//   // Auto import components (https://go.nuxtjs.dev/config-components)
+//   components: true,
+
+//   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+//   buildModules: [
+//     // https://go.nuxtjs.dev/eslint
+//     '@nuxtjs/eslint-module',
+//   ],
+
+//   // Modules (https://go.nuxtjs.dev/config-modules)
+//   modules: [
+//     // https://go.nuxtjs.dev/content
+//     '@nuxt/content',
+//   ],
+
+//   // Build Configuration (https://go.nuxtjs.dev/config-build)
+//   build: {},
+//   hooks: {
+//     'content:file:beforeInsert': async (document, database) => {
+//       if (document.extension === '.json' && document.body) {
+//         const data = await database.markdown.toJSON(document.body)
+
+//         Object.assign(document, data)
+//       }
+//     },
+//   },
+//   content: {
+//     nestedProperties: ['categories.slug'],
+//     extendParser: {
+//       '.custom': (file) => ({
+//         body: file.split('\n').map((line) => line.trim()),
+//       }),
+//     },
+//   },
+// }
+
+import theme from '@nuxt/content-theme-docs'
+
+export default theme({
+  docs: {
+    primaryColor: '#E24F55',
   },
-
-  // Auto import components (https://go.nuxtjs.dev/config-components)
-  components: true,
-
-  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-  buildModules: [
-    // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
-  ],
-
-  // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: [
-    // https://go.nuxtjs.dev/content
-    '@nuxt/content',
-  ],
-
-  // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {},
-  hooks: {
-    'content:file:beforeInsert': async (document, database) => {
-      if (document.extension === '.json' && document.body) {
-        const data = await database.markdown.toJSON(document.body)
-
-        Object.assign(document, data)
-      }
-    },
-  },
-  content: {
-    nestedProperties: ['categories.slug'],
-    extendParser: {
-      '.custom': (file) => ({
-        body: file.split('\n').map((line) => line.trim()),
-      }),
-    },
-  },
-}
+})
